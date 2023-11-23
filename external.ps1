13c53bb574c3a8e53810b57850ab5d83126cba2b
thomasonsignup <37800505+thomasonsignup@users.noreply.github.com>
2023-11-23 15:28:42 +0100

    Update external.ps1

--- external.ps1
+++ external.ps1
@@ -1,6 +1,7 @@
+param([string] $azureADApplicationName)
 $Headers = @{Authorization = "Bearer $((Get-AzAccessToken -ResourceUrl 'https://graph.microsoft.com/').Token)" }
-$AppDisplayName = "SignupSoftwareAB-ExFlowCloud-FO-72b08bde-b1b7-4138-a615-9f6790947849"
-$App = Invoke-RestMethod -Method GET -Uri "https://graph.microsoft.com/v1.0/applications?`$filter=displayName eq '$($AppDisplayName)'" -Headers $Headers -ContentType "application/json"
+
+$App = Invoke-RestMethod -Method GET -Uri "https://graph.microsoft.com/v1.0/applications?`$filter=displayName eq '$($AazureADApplicationName)'" -Headers $Headers -ContentType "application/json"
 
 If ([string]::IsNullOrEmpty($app.value)) {
     $output = "Create new app"
