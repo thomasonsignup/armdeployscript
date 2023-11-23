12a7d874a6213d5c5a21884d6bf2510cdd97d8d4
thomasonsignup <37800505+thomasonsignup@users.noreply.github.com>
2023-11-23 13:09:40 +0100

    Update external.ps1

--- external.ps1
+++ external.ps1
@@ -3,11 +3,12 @@ $AppDisplayName = "SignupSoftwareAB-ExFlowCloud-FO-72b08bde-b1b7-4138-a615-9f679
 $App = Invoke-RestMethod -Method GET -Uri "https://graph.microsoft.com/v1.0/applications?`$filter=displayName eq '$($AppDisplayName)'" -Headers $Headers -ContentType "application/json"
 
 If ([string]::IsNullOrEmpty($app.value)) {
-    Write-Output "Create new app"
     $output = "Create new app"
+    Write-Output $output
 } else {
-    Write-Output "Fix existing"
     $output = "Fix existing"
+    Write-Output $output
+    
 }
 
 $DeploymentScriptOutputs = @{}
