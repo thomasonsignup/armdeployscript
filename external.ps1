7db016a42df4d0040d15fe9a38a7a924e3a4844d
thomasonsignup <37800505+thomasonsignup@users.noreply.github.com>
2023-11-23 17:33:34 +0100

    Update external.ps1

--- external.ps1
+++ external.ps1
@@ -1,19 +1,12 @@
 $Headers = @{Authorization = "Bearer $((Get-AzAccessToken -ResourceUrl 'https://graph.microsoft.com/').Token)" }
-$AppDisplayName = "SignupSoftwareAB-ExFlowCloud-FO-72b08bde-b1b7-4138-a615-9f6790947849"
-$EnvOutput = ${Env:AzureADApplicationName}
-Write-output "this is verification"
-Write-Output $EnvOutput
-Write-Output ${Env:AzureADApplicationName}
-Write-Output $Env:AzureADApplicationName
-if (!($EnvOutput)) { 
-    $EnvOutput = "no environment output" 
+if ($EnvOutput) { 
+    $EnvOutput = "Environment var exists" 
 }
 else { 
-    Write-Output $EnvOutput
-    $EnvOutput = "environment output hit else clause" 
+    $EnvOutput = "Environment var no exists" 
 }
 $App = Invoke-RestMethod -Method GET -Uri "https://graph.microsoft.com/v1.0/applications?`$filter=displayName eq '$($AppDisplayName)'" -Headers $Headers -ContentType "application/json"
-
+$AppOutput = $app | ConvertTo-Json
 If ([string]::IsNullOrEmpty($app.value)) {
     $output = "Create new app"
     Write-Output $output
@@ -26,3 +19,4 @@ else {
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['AppStatus'] = $output
 $DeploymentScriptOutputs['EnvOutput'] = $EnvOutput
+$DeploymentScriptOutputs['AppOutput'] = $AppOutput
