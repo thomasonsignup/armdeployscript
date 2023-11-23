2f40b5656eb6bee008b216fa7dabbcc73fdc3ad6
thomasonsignup <37800505+thomasonsignup@users.noreply.github.com>
2023-11-23 16:30:26 +0100

    Update external.ps1

--- external.ps1
+++ external.ps1
@@ -2,7 +2,7 @@ $Headers = @{Authorization = "Bearer $((Get-AzAccessToken -ResourceUrl 'https://
 $AppDisplayName = "SignupSoftwareAB-ExFlowCloud-FO-72b08bde-b1b7-4138-a615-9f6790947849"
 $EnvOutput = ${Env:AzureADApplicationName}
 $EnvOutput = "dummystring"
-if (!($EnvOutput)) {$EnvOutput = "no environment output"}
+if (!($EnvOutput)) {$EnvOutput = "no environment output"} else {$EnvOutput = "environment output hit else clause"}
 $App = Invoke-RestMethod -Method GET -Uri "https://graph.microsoft.com/v1.0/applications?`$filter=displayName eq '$($AppDisplayName)'" -Headers $Headers -ContentType "application/json"
 
 If ([string]::IsNullOrEmpty($app.value)) {
