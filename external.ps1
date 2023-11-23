69693a9be7288c62530ceb0f7100ce499dd5f4b8
thomasonsignup <37800505+thomasonsignup@users.noreply.github.com>
2023-11-23 16:39:06 +0100

    Update external.ps1

--- external.ps1
+++ external.ps1
@@ -1,13 +1,21 @@
 $Headers = @{Authorization = "Bearer $((Get-AzAccessToken -ResourceUrl 'https://graph.microsoft.com/').Token)" }
 $AppDisplayName = "SignupSoftwareAB-ExFlowCloud-FO-72b08bde-b1b7-4138-a615-9f6790947849"
 $EnvOutput = ${Env:AzureADApplicationName}
-if (!($EnvOutput)) {$EnvOutput = "no environment output"} else {$EnvOutput = "environment output hit else clause"}
+Write-output "this is verification"
+if (!($EnvOutput)) { 
+    $EnvOutput = "no environment output" 
+}
+else { 
+    Write-Output $EnvOutput
+    $EnvOutput = "environment output hit else clause" 
+}
 $App = Invoke-RestMethod -Method GET -Uri "https://graph.microsoft.com/v1.0/applications?`$filter=displayName eq '$($AppDisplayName)'" -Headers $Headers -ContentType "application/json"
 
 If ([string]::IsNullOrEmpty($app.value)) {
     $output = "Create new app"
     Write-Output $output
-} else {
+}
+else {
     $output = "Fix existing"
     Write-Output $output
 }
