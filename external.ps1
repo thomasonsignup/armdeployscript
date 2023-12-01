741bd7d2d3c06f09bd221a53de3ea4fac5871bed
thomasonsignup <37800505+thomasonsignup@users.noreply.github.com>
2023-12-01 10:46:22 +0100

    Update external.ps1

--- external.ps1
+++ external.ps1
@@ -1,6 +1,12 @@
-
+param(
+    [string]$Dynamics,
+    [string]$TenantId,
+    [string]$Appid,
+    [string]$customerName,
+    [string]$dnsDomainName
+)
 $Headers = @{Authorization = "Bearer $((Get-AzAccessToken -ResourceUrl 'https://graph.microsoft.com/').Token)" }
-$AppDisplayName = "SignupSoftwareAB-ExFlowCloud-$Dynamics-$TenantId"
+$AppDisplayName = "SignupSoftwareAB-ExFlowCloud-$ERP-$TenantId"
 $CustomerUrl = "https://" + $customerName + ".$dnsDomainName/signin-oidc"
 $App = (Invoke-RestMethod -Method GET -Uri "https://graph.microsoft.com/v1.0/applications?`$filter=displayName eq '$($AppDisplayName)'" -Headers $Headers -ContentType "application/json").value
 
@@ -12,7 +18,7 @@ If (-not($app)) {
     $GraphAccess = '{"resourceAppId":"00000003-0000-0000-c000-000000000000","resourceAccess":[{"id":"e1fe6dd8-ba31-4d61-89e7-88639da4683d","type":"Scope"},{"id":"b340eb25-3456-403f-be2f-af7a0d370277","type":"Scope"}]}' | ConvertFrom-Json
 
     # ERP Permissions
-    Switch ($Dynamics) {
+    Switch ($ERP) {
         BCOnprem { $ERPAccess = "" <#No resource access needed (yet?)#> }
         BC { $ERPAccess = '{"resourceAppId":"996def3d-b36c-4153-8607-a6fd3c01b89f","resourceAccess":[{"id":"a42b0b75-311e-488d-b67e-8fe84f924341","type":"Role"}]}' } 
         FO { $ERPAccess = '{"resourceAppId":"00000015-0000-0000-c000-000000000000","resourceAccess":[{"id":"6397893c-2260-496b-a41d-2f1f15b16ff3","type":"Scope"},{"id":"a849e696-ce45-464a-81de-e5c5b45519c1","type":"Scope"},{"id":"ad8b4a5c-eecd-431a-a46f-33c060012ae1","type":"Scope"}]}' } 
